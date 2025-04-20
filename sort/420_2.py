e2484b6cfdb316ac8888bfc3ad99eee741662969
Ind1ex1 <2777904242@qq.com>
2025-04-20 14:05:06 +0800

    419/420

---
+++ sort/420_2.py
@@ -0,0 +1,20 @@
+# #给你一个 无重叠的 ，按照区间起始端点排序的区间列表 intervals，其中 intervals[i] = [starti, endi] 表示第 i 个区间的开始和结束，并且 intervals 按照 starti 升序排列。同样给定一个区间 newInterval = [start, end] 表示另一个区间的开始和结束。
+#
+# 在 intervals 中插入区间 newInterval，使得 intervals 依然按照 starti 升序排列，且区间之间不重叠（如果有必要的话，可以合并区间）。
+#
+# 返回插入之后的 intervals。
+#
+# 注意 你不需要原地修改 intervals。你可以创建一个新数组然后返回它。
+class Solution:
+    def insert(self, intervals: List[List[int]], newInterval: List[int]) -> List[List[int]]:
+        intervals.append(newInterval)
+        intervals.sort(key=lambda x: (x[0], x[1]))
+
+        merged = []
+        for interval in intervals:
+            if not merged or merged[-1][1] < interval[0]:
+                merged.append(interval)
+            else:
+                merged[-1][1] = max(merged[-1][1], interval[1])
+
+        return merged
